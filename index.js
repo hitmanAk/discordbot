683546abf79907cc308a51582c22bc71b2a4b4a1
Saleh Alikhani <salehalikhani@gmail.com>
2025-02-06 00:23:46 +0330

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+import { Client, GatewayIntentBits } from 'discord.js'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+  ],
+})
+
+client.login(process.env.DISCORD_TOKENS)
+
+client.on('messageCreate', async (message) => {
+  console.log(message)
+  if (!message.author.bot) {
+    message.author.send(`Echo ${message.content}`)
+  }
+})
---
+++ register-commands.js
@@ -0,0 +1,20 @@
+import { REST,Routes } from "discord.js";
+
+const commands = [
+    {
+        name:'hey',
+        descriptions:'Replies with hey'
+    }
+]
+
+const rest = new REST({})
+
+
+(async()=>{
+    try{
+
+    }
+    catch (error){
+        console.log(error)
+    }
+})
